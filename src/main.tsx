40d250a1b9a6fd2419db00964fb1aa9dd2363c12
Jimoh4Tech <olamide14044@gmail.com>
2025-02-18 21:26:20 +0100

    feat(analytics): add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import theme from './theme.ts';
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import theme from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<ChakraProvider theme={theme}>
-			<BrowserRouter>
-			<App />
-			</BrowserRouter>
-		</ChakraProvider>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+        <Analytics />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
